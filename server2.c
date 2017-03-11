2d55a82138d49be71b9356d05eb4047412803c7d
mangi07 <mangi07@users.noreply.github.com>
2017-03-10 20:53:42 -0600

    still refactoring server2.c, prepare to integrate with connectionController.c

--- server2.c
+++ server2.c
@@ -26,8 +26,11 @@ Protocol: first 19 bytes will be a string representation of
 
 
 /*
-TODO: define function to recv
+TODO: declare functions defined at the bottom
 */
+int bindAndListen( const char* PORT );
+int acceptCient( int sockfd );
+
 
 void sigchld_handler(int s)
 {
@@ -52,86 +55,13 @@ void *get_in_addr(struct sockaddr *sa)
 
 int main(void)
 {
-	int sockfd, new_fd;  // listen on sock_fd, new connection on new_fd
-	struct addrinfo hints, *servinfo, *p;
-	struct sockaddr_storage their_addr; // connector's address information
-	socklen_t sin_size;
-	struct sigaction sa;
-	int yes=1;
-	char s[INET6_ADDRSTRLEN];
-	int rv;
-
-	memset(&hints, 0, sizeof hints);
-	//hints.ai_family = AF_UNSPEC;
-	hints.ai_family = AF_INET; // IPv4
-	hints.ai_socktype = SOCK_STREAM;
-	hints.ai_flags = AI_PASSIVE; // use my IP
-
-	if ((rv = getaddrinfo(NULL, "3490", &hints, &servinfo)) != 0) {
-		fprintf(stderr, "getaddrinfo: %s\n", gai_strerror(rv));
-		return 1;
-	}
-
-	// loop through all the results and bind to the first we can
-	for(p = servinfo; p != NULL; p = p->ai_next) {
-		if ((sockfd = socket(p->ai_family, p->ai_socktype,
-				p->ai_protocol)) == -1) {
-			perror("server: socket");
-			continue;
-		}
-
-		if (setsockopt(sockfd, SOL_SOCKET, SO_REUSEADDR, &yes,
-				sizeof(int)) == -1) {
-			perror("setsockopt");
-			exit(1);
-		}
-
-		if (bind(sockfd, p->ai_addr, p->ai_addrlen) == -1) {
-			close(sockfd);
-			perror("server: bind");
-			continue;
-		}
-
-		break;
-	}
-
-	freeaddrinfo(servinfo); // all done with this structure
-
-	if (p == NULL)  {
-		fprintf(stderr, "server: failed to bind\n");
-		exit(1);
-	}
-
-	if (listen(sockfd, BACKLOG) == -1) {
-		perror("listen");
-		exit(1);
-	}
-
-	sa.sa_handler = sigchld_handler; // reap all dead processes
-	sigemptyset(&sa.sa_mask);
-	sa.sa_flags = SA_RESTART;
-	if (sigaction(SIGCHLD, &sa, NULL) == -1) {
-		perror("sigaction");
-		exit(1);
-	}
-
-	printf("server: waiting for connections...\n");
-
+	int sockfd = bindAndListen( "3490" ); // TODO: test this!!
+	
+	
 	while(1) {  // main accept() loop
-		sin_size = sizeof their_addr;
-		printf( "DEBUG: first\n" );
-		new_fd = accept(sockfd, (struct sockaddr *)&their_addr, &sin_size);
-		printf( "DEBUG: new_fd: %d\n", new_fd );
-		if (new_fd == -1) {
-			perror("accept");
-			continue;
-		}
-
-		inet_ntop(their_addr.ss_family,
-			get_in_addr((struct sockaddr *)&their_addr),
-			s, sizeof s);
-		printf("server: got connection from %s\n", s);
-
+		int new_fd = acceptCient( sockfd );
+		// TODO: check for return value of -1 and close sockfd as well as new_fd
+		
 		if (!fork()) { // this is the child process
 			close(sockfd); // child doesn't need the listener
 			// receive command
@@ -154,14 +84,11 @@ int main(void)
 Description: Open a new socket with given host and port number
 Return: socket file descriptor of listening socket
 */
-void bindAndListen( const char* host, const char* PORT ){
+int bindAndListen( const char* PORT ){
 	int sockfd;  // listen on sock_fd
 	struct addrinfo hints, *servinfo, *p;
-	// struct sockaddr_storage their_addr; // connector's address information
-	//socklen_t sin_size;
 	struct sigaction sa;
 	int yes=1;
-	//char s[INET6_ADDRSTRLEN];
 	int rv;
 
 	memset(&hints, 0, sizeof hints);
@@ -217,6 +144,7 @@ void bindAndListen( const char* host, const char* PORT ){
 		exit(1);
 	}
 
+	return sockfd;
 }
 
 /*
@@ -231,7 +159,7 @@ int acceptCient( int sockfd ){
 	
 	sin_size = sizeof their_addr;
 	new_fd = accept(sockfd, (struct sockaddr *)&their_addr, &sin_size);
-	printf( "DEBUG: new_fd: %d\n", new_fd );
+	printf( "server: new_fd: %d\n", new_fd );
 	if (new_fd == -1) {
 		perror("accept");
 		return -1;
